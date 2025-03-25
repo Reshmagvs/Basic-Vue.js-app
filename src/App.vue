fce55111191c6bceddf3aa1fb3eef28443e2ab53
ReshmaGVS <gvsreshma2005@gmail.com>
2025-03-25 22:10:52 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Vue Task List App</h1>
+    <TaskList />
+  </div>
+</template>
+
+<script>
+import TaskList from "./components/TaskList.vue";
+
+export default {
+  components: {
+    TaskList,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  text-align: center;
+}
+</style>
+
---
+++ src/components/TaskList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+      <h2>Task List</h2>
+      <input v-model="newTask" placeholder="Enter a task" />
+      <button @click="addTask">Add Task</button>
+  
+      <ul>
+        <li v-for="(task, index) in tasks" :key="index">
+          {{ task }}
+          <button @click="removeTask(index)">Remove</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        newTask: "",
+        tasks: [],
+      };
+    },
+    methods: {
+      addTask() {
+        if (this.newTask.trim()) {
+          this.tasks.push(this.newTask);
+          this.newTask = "";
+        }
+      },
+      removeTask(index) {
+        this.tasks.splice(index, 1);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  button {
+    margin-left: 10px;
+    cursor: pointer;
+  }
+  </style>
+  
